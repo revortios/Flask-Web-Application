e5156b79b5723255bc5ff0b0587ebe620adad871
revortios <61480534+revortios@users.noreply.github.com>
2024-06-13 18:43:59 +0200

    login and signup logic added

--- Flask Web Application/Website/__init__.py
+++ Flask Web Application/Website/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'the eyes never lie chico'
+    app.config
     
     from .views import views
     from .auth import auth
