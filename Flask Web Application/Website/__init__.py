83fc51f7b02ceb7990814ec1dd3a85466524ce76
revortios <61480534+revortios@users.noreply.github.com>
2024-06-13 22:31:01 +0200

    imported sql and made 2 classes , User and Note gave both user and note class their own attributes

--- Flask Web Application/Website/__init__.py
+++ Flask Web Application/Website/__init__.py
@@ -4,10 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'the eyes never lie chico'
-    app.config
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
