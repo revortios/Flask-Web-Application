4d6383bb4c260a7646abbdf33fe3e0f0cc68ddf1
revortios <61480534+revortios@users.noreply.github.com>
2024-06-14 19:07:54 +0200

    Authentication and Models made for Sign Up and Login

--- Flask Web Application/Website/models.py
+++ Flask Web Application/Website/models.py
@@ -7,7 +7,7 @@ class Note(db.Model):
     data = db.Column(db.String(10000))
     data = db.Column(db.DateTime(timezone=True), default=func.now)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    
+
 
 
 class User(db.Model, UserMixin):
