fdd9788b277b5f053caaed9293fb9dda6354639c
revortios <61480534+revortios@users.noreply.github.com>
2024-06-14 23:38:19 +0200

    "Finished"

--- Flask Web Application/Website/models.py
+++ Flask Web Application/Website/models.py
@@ -2,12 +2,12 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
-    data = db.Column(db.DateTime(timezone=True), default=func.now)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
 
 
 class User(db.Model, UserMixin):
@@ -16,15 +16,3 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
-    
-
-
-
-
-
-
-
-
-
-
-
